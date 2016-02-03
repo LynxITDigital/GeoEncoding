d25a21ec0f7270b1e9c758cbe1dec0b68c1f27c6
Oliver Jacobs <oliver.jacobs1@icloud.com>
2016-02-03 15:31:55 +1100

    Fixed Flux Router to work with Redux - using 'replace' instead of switch tabs

--- app/actions/routerActions.js
+++ app/actions/routerActions.js
@@ -1,18 +1,32 @@
+var actions = exports = module.exports
+
 exports.ON_PUSH = "ON_PUSH";
 exports.ON_POP = "ON_POP";
+exports.ON_REPLACE = "ON_REPLACE";
 
-exports.onPush = function onPush(route, routerState){
+exports.onPush = function onPush(route){
+  console.log("PUSH ACTION");
   return {
-    type: exports.ON_PUSH,
-    route: route,
-    routerState: routerState
+    type: actions.ON_PUSH,
+    route: route
   }
 }
 
 
-exports.onPop = function onPop(route, routerState){
+exports.onPop = function onPop(){
+  console.log("POP ACTION");
   return {
-    type: exports.ON_POP,
-    routerState: routerState
+    type: actions.ON_POP
+  }
+}
+
+
+
+
+exports.onReplace = function onPop(route){
+  console.log("REPLACE ACTION");
+  return {
+    type: actions.ON_REPLACE,
+    route: route
   }
 }
