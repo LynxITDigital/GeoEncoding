67853149c3077a66668bc1ffcb2f8227b1157ac7
Oliver Jacobs <oliver.jacobs1@icloud.com>
2016-02-10 16:51:06 +1100

    Styled Android NavBar to look similar to iOS NavBar

--- app/containers/geoEncodingApp.js
+++ app/containers/geoEncodingApp.js
@@ -16,9 +16,11 @@ import { connect } from 'react-redux/native';
 // } from 'react-native-router-redux';
 
 import {Router, Route, Schema, Animations, TabBar, Actions} from 'react-native-router-flux'
+import {ExRouter} from 'react-native-router-flux/ExRouter'
 import * as styles from './RouterContainerStyles';
 import TabBarItem from '../components/TabBarItem';
 import * as assets from '../../assets';
+
 
 //import TabBar from '../components/tabBar';
 
@@ -32,6 +34,8 @@ import AddressDetails from '../components/addressDetails';
 import Launch from '../components/launch';
 import VideoPage from '../components/videoPage';
 import DownloadList from '../components/downloadList';
+
+import NavigationBar from '../components/navBar';
 
 const mapStateToProps = state => ({
   addresses : state.addressesByGeoEncoding.addresses,
@@ -81,18 +85,26 @@ class GeoEncodingApp extends Component {
             }
         });
 
+//leftButtonStyle={{width: 0}} barButtonTextStyle={{color: "#090"}}
 
      return(<Router hideNavBar={true}
+       navigationBarStyle={styles.navBarStyle} //Nav Bar Container
+       barButtonTextStyle={{color: "#090"}} //No Effect?
+       //style={{backgroundColor: "#090"}} //Behing Nav Bar / Behind Tab Bar
+       //sceneStyle={{backgroundColor: "#090"}} //Entire background
+
+       titleStyle={styles.navTextStyle} //Main Title Text
+       barButtonIconStyle={styles.barButtonIconStyle} // E.g. Back button
        onPush={(route)=>{this.props.routerActions.onPush(route.name); return true}}
        onPop={()=>{this.props.routerActions.onPop(); return true}}
        onReplace={(route)=>{this.props.routerActions.onReplace(route.name); return true}}
        >
-         <Schema name="modal" sceneConfig={Animations.FlatFloatFromRight}/>
+         <Schema name="modal" sceneConfig={Animations.FlatFloatFromRight} hideNavBar={false}/>
          <Schema name="default" sceneConfig={Animations.FlatFloatFromRight}/>
          <Schema name="tab" icon={TabBarItem} type="replace" hideNavBar={false} />
          <Schema name="withoutAnimation"/>
 
-         <Route name="tabbar" hideNavBar={true}>
+         <Route name="tabbar" hideNavBar={true} >
              <Router hideNavBar={true} footer={TabBar} tabBarStyle={styles.getTabBarStyle(this.props)} sceneStyle={styles.sceneStyle}
                      onPush={(route)=>{this.props.routerActions.onPush(route.name); return true}}
                      onPop={()=>{this.props.routerActions.onPop(); return true}}
@@ -102,11 +114,11 @@ class GeoEncodingApp extends Component {
                <Route name="launch"  schema="tab" component={addrComp} title="Geo Encoding" tabBarItem={{icon: assets.home, title: 'Geo Encoding'}}  initial={true} />
                <Route name="favourites" schema="tab" component={favComp} title="Favourites" tabBarItem={{icon: assets.favourites, title: 'Favourites'}} />
                <Route name="video" schema="tab" component={VideoPage} title="Video" tabBarItem={{icon: assets.video, title: 'Video'}}/>
-               <Route name="download" schema="tab" component={dlComp} hideNavBar={false} tabBarItem={{icon: assets.download, title: 'Download'}}/>
+               <Route name="download" schema="tab" component={dlComp} title="Download" tabBarItem={{icon: assets.download, title: 'Download'}}/>
              </Router>
           </Route>
-          <Route name="details" component={AddressDetails} hideNavBar={false}  title="Details" schema="modal"/>
-          <Route name="dlvideo" schema="modal" component={VideoPage} hideNavBar={false} title="Downloaded Video"/>
+          <Route name="details" component={AddressDetails}  title="Details" schema="modal"/>
+          <Route name="dlvideo" schema="modal" component={VideoPage}  title="Downloaded Video"/>
 
 
      </Router>
