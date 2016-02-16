932f75a712bf066c7c3c0e869f587866b086524a
StephenPrayogo <stephenprayogo@gmail.com>
2016-02-17 10:30:43 +1100

    Initial commit
    Initial commit. Android packages imports have not been set up properly yet.

--- android/app/src/main/java/com/geoencoding/MainActivity.java
+++ android/app/src/main/java/com/geoencoding/MainActivity.java
@@ -1,128 +1,56 @@
 package com.geoencoding;
 
-import android.app.Activity;
-import com.imagepicker.ImagePickerPackage;
-import com.github.yamill.orientation.OrientationPackage;
-import com.brentvatne.react.ReactVideoPackage;
-import com.AirMaps.AirPackage;
-import com.remobile.splashscreen.RCTSplashScreenPackage;
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.view.KeyEvent;
-import android.content.Intent;
-import android.content.res.Configuration;
-
-import com.facebook.react.LifecycleState;
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.ReactRootView;
-import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
-import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
-
-import org.pgsqlite.SQLitePluginPackage;
-
-import com.microsoft.codepush.react.CodePush;
-import android.support.v4.app.FragmentActivity;
-
-import com.rnfs.RNFSPackage;
-
+import com.facebook.react.ReactActivity;
+import com.brentvatne.RCTVideo.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.github.yamill.orientation.OrientationPackage;
+import com.AirMaps.AirPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.rnfs.RNFSPackage;
+import com.microsoft.codepush.react.CodePushReactPackage;
+import com.remobile.splashscreen.RCTSplashScreenPackage;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 
+import java.util.Arrays;
+import java.util.List;
 
-public class MainActivity extends FragmentActivity implements DefaultHardwareBackBtnHandler {
+public class MainActivity extends ReactActivity {
 
-  private ReactInstanceManager mReactInstanceManager;
-  private ReactRootView mReactRootView;
-  private ImagePickerPackage mImagePicker;
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    CodePush codePush = new CodePush("9OcY7WuRIwcigP2x6H5z8bTZPLN94yXS9p7Fg", this, BuildConfig.DEBUG);
-
-    mReactRootView = new ReactRootView(this);
-
-
-      // instantiate package
-      mImagePicker = new ImagePickerPackage(this);
-
-
-    mReactInstanceManager = ReactInstanceManager.builder()
-                .setApplication(getApplication())
-                // .setBundleAssetName("index.android.bundle")
-                .setJSBundleFile(codePush.getBundleUrl("index.android.bundle"))
-                .setJSMainModuleName("index.android")
-                .addPackage(new MainReactPackage())
-                .addPackage(new ImagePickerPackage(this))
-                .addPackage(new OrientationPackage(this))
-                .addPackage(new ReactVideoPackage())
-                .addPackage(new AirPackage())
-                .addPackage(new RCTSplashScreenPackage(this))
-                .addPackage(new SQLitePluginPackage(this))   // register SQLite Plugin here
-                .addPackage(new RNFSPackage())
-                .addPackage(new VectorIconsPackage())
-                .addPackage(codePush.getReactPackage())
-                .setUseDeveloperSupport(BuildConfig.DEBUG)
-                .setInitialLifecycleState(LifecycleState.RESUMED)
-                .build();
-
-    mReactRootView.startReactApplication(mReactInstanceManager, "GeoEncoding", null);
-
-    setContentView(mReactRootView);
-  }
-
-  @Override
-  public boolean onKeyUp(int keyCode, KeyEvent event) {
-    if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
-      mReactInstanceManager.showDevOptionsDialog();
-      return true;
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "GeoEncoding";
     }
-    return super.onKeyUp(keyCode, event);
-  }
 
-  @Override
-  public void onBackPressed() {
-    if (mReactInstanceManager != null) {
-        mReactInstanceManager.onBackPressed();
-    } else {
-      super.onBackPressed();
+    /**
+     * Returns whether dev mode should be enabled.
+     * This enables e.g. the dev menu.
+     */
+    @Override
+    protected boolean getUseDeveloperSupport() {
+        return BuildConfig.DEBUG;
     }
-  }
 
-  @Override
-  public void invokeDefaultOnBackPressed() {
-    super.onBackPressed();
-  }
-
-  @Override
-  protected void onPause() {
-    super.onPause();
-
-    if (mReactInstanceManager != null) {
-      mReactInstanceManager.onPause();
+   /**
+   * A list of packages used by the app. If the app uses additional views
+   * or modules besides the default ones, add more packages here.
+   */
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+        new MainReactPackage(),
+        new ReactVideoPackage(),
+        new VectorIconsPackage(),
+        new OrientationPackage(),
+        new AirPackage(),
+        new ImagePickerPackage(),
+        new RNFSPackage(),
+        new CodePushReactPackage(),
+        new RCTSplashScreenPackage()
+      );
     }
-  }
-
-  @Override
-  protected void onResume() {
-    super.onResume();
-
-      if (mReactInstanceManager != null) {
-      mReactInstanceManager.onResume(this, this);
-    }
-  }
-
-  @Override
-  public void onConfigurationChanged(Configuration newConfig) {
-    super.onConfigurationChanged(newConfig);
-    Intent intent = new Intent("onConfigurationChanged");
-    intent.putExtra("newConfig", newConfig);
-    this.sendBroadcast(intent);
-  }
-
-  @Override
-  public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-      super.onActivityResult(requestCode, resultCode, data);
-      mImagePicker.handleActivityResult(requestCode, resultCode, data);
-  }
 }
---
+++ scripts/removefbjs.sh
@@ -0,0 +1,1 @@
+find . -name 'fbjs' -print | grep "\./node_modules/fbjs" -v | xargs rm -rf
